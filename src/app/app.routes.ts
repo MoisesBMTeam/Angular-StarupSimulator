558f4455bae4342deb264896e4ee46936a46d2dd
Milo443 <104799754+Milo443@users.noreply.github.com>
2024-05-01 09:40:46 -0500

    creacion componente creacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { CreacionComponent } from './components/simulator/creacion/creacion.component';
+
+export const routes: Routes = [
+
+    {path:'',component: CreacionComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
